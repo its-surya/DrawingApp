bbd7f886695f2dc8d3d55a4eb39a33d2c1dd1730
Suryakant Panda <suryakant.panda10@gmail.com>
2023-09-29 14:22:27 +0530

    comment

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
-    }
+    }//hii
 
     private fun showBrushSizeChooserDialog() {
 
